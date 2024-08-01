872bd8adb5ddd112c92a996c5a8d5d4ce06a4339
amjadmajid <amjad.y.majid@gmail.com>
2024-08-02 00:26:53 +0200

    change the training loop and logging

--- trainer.py
+++ trainer.py
@@ -6,13 +6,12 @@ import time
 from utils import evaluate_model, save_model, count_parameters, load_model_weights_
 from dataspace import DataLoaderLite
 from config import ConfigHandler
-from llm_model import EncoderTransformerModel
+from llm_model import TransformerModel
 from pathlib import Path
 import argparse
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 ## Load configurations
 try:
@@ -24,41 +23,16 @@ except Exception as e:
 
 logger.info(f"The selected device is {config.device}")
 
-# initialize data loaders
-training_data = DataLoaderLite(config.micro_batch_size, config.seq_len, 0, 1, 'train')
-val_data      = DataLoaderLite(config.micro_batch_size, config.seq_len, 0, 1, 'val')
-
-torch.set_float32_matmul_precision('high')
-
-# initialize the model
-model = EncoderTransformerModel(
-    n_head=config.n_head,
-    vocab_size=config.vocab_size,
-    n_embd=config.n_embd, 
-    seq_len=config.seq_len, 
-    device=config.device,
-    dropout_rate=config.embd_pdrop, 
-    n_blocks=config.n_blocks 
-).to(config.device)
-
-# model = torch.compile(model)
-
-# initialize optimizer and loss
-optimizer = torch.optim.AdamW(model.parameters(), lr=config.lr)
-criterion = nn.CrossEntropyLoss()
-
-logger.info(f"The model has {count_parameters(model)} trainable parameters")
-
-def train(continue_training, config): 
-    max_loss = float('inf')
-    train_losses = 0
+def train(model, optimizer, criterion ,continue_training, config): 
 
     if continue_training:
         # load checkpointed configuration and model weights
-        config = ConfigHandler.load(config.ckpt_dir+"/"+config.ckpt_config)
-        load_model_weights_(model, config.ckpt_dir+"/"+config.ckpt_model, config.device)
+        config = ConfigHandler.load(Path(config.ckpt_dir) / Path(config.ckpt_config))
+        load_model_weights_(model, Path(config.ckpt_dir) / Path(config.ckpt_model), config.device) # update model weights inplace 
         logger.info("Loaded model's weights")
 
+    train_losses = 0
+    model = torch.compile(model)
     start_time = time.time()
 
     for iteration in range(1, config.max_iter+1):
@@ -72,17 +46,13 @@ def train(continue_training, config):
             X, Y = training_data.next_batch()
             X = torch.tensor(X, dtype=torch.long).to(config.device)
             Y = torch.tensor(Y, dtype=torch.long).to(config.device)
-            # X, Y = training_data.get_rand_batch(config.batch_size, config.seq_len)
-            # logger.info(f"Training batch shapes - X: {X.shape}, Y: {Y.shape}")
             with torch.autocast(device_type=config.device, dtype=torch.bfloat16):
                 logits = model(X)
-                # logger.info(f"Logits shape: {logits.shape}")
 
                 loss = criterion(logits.view(-1, logits.size(-1)), Y.view(-1))
                 loss = loss / grad_accumulations
             accum_loss += loss.item()
             loss.backward()
-            # logger.info(f"Loss: {loss.item()}")
 
         # gradient clipping
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
@@ -90,35 +60,60 @@ def train(continue_training, config):
         optimizer.step()
         train_losses += accum_loss
 
-        if iteration  % config.eval_inter == 0:
-
+        if iteration % config.log_inter == 0:
             # processing speed logging
             end_time = time.time() 
-            dt =  (end_time - start_time ) / config.eval_inter
+            dt =  (end_time - start_time ) / config.log_inter
             start_time = end_time
             processed_tokens = config.batch_size * config.seq_len
             tokens_per_sec = processed_tokens // dt 
-
             # loss logging
-            train_loss = train_losses / config.eval_inter
+            train_loss = train_losses / config.log_inter
             train_losses = 0
+
+            logger.info(f"Iteration {iteration} | Train Loss {train_loss:.5f} |  dt {dt * 1000:.3f} ms | {tokens_per_sec} tokens/sec")
+
+        if iteration  % config.eval_inter == 0:
             # TODO: put with torch.no_grad() here. Outside the function
             val_loss = evaluate_model(model,val_data, criterion, config)
-            logger.info(f"Iteration {iteration} | Train Loss {train_loss:.5f} | Validation Loss {val_loss:.5f} | dt {dt * 1000:.3f} ms | {tokens_per_sec} tokens/sec")
-
-            # checkpointing
-            if val_loss < max_loss: 
-                max_loss = val_loss
-                ckpt_model_path =  Path(config.ckpt_dir) / Path(config.ckpt_model)
-                ckpt_config_path =  Path(config.ckpt_dir) / Path(config.ckpt_config)
-                save_model(model, ckpt_model_path )
-                config.save(ckpt_config_path)
-                logger.info(f"{iteration} - Model saved to {ckpt_model_path}; loss: {val_loss:.4f}")
-
+            logger.info(f"Iteration {iteration} | Validation Loss {val_loss:.5f} ")
+        
+        # checkpointing
+        if accum_loss < config.max_loss: 
+            config.max_loss = accum_loss
+            ckpt_model_path =  Path(config.ckpt_dir) / Path(config.ckpt_model)
+            ckpt_config_path =  Path(config.ckpt_dir) / Path(config.ckpt_config)
+            save_model(model, ckpt_model_path )
+            config.save(ckpt_config_path)
+            logger.info(f"{iteration} - Model saved to {ckpt_model_path}; loss: {accum_loss:.4f}")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="BabyGPT script for LLM training")
     parser.add_argument('-ct', '--continue-training', action='store_true', help='Flag to continue training from a saved model state')
     args = parser.parse_args()
 
-    train(args.continue_training, config)
+    # initialize data loaders
+    training_data = DataLoaderLite(config.micro_batch_size, config.seq_len, 0, 1, 'train')
+    val_data      = DataLoaderLite(config.micro_batch_size, config.seq_len, 0, 1, 'val')
+
+    torch.set_float32_matmul_precision('high')
+
+    # initialize the model
+    model = TransformerModel(
+        n_head=config.n_head,
+        vocab_size=config.vocab_size,
+        n_embd=config.n_embd, 
+        seq_len=config.seq_len, 
+        device=config.device,
+        dropout_rate=config.dropout_rate, 
+        n_blocks=config.n_blocks,
+        decoder=True 
+    ).to(config.device)
+
+    # initialize optimizer and loss
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config.lr)
+    criterion = nn.CrossEntropyLoss()
+
+    logger.info(f"The model has {count_parameters(model)} trainable parameters")
+
+    train(model, optimizer, criterion, args.continue_training, config)
