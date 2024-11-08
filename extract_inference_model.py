a2e8b9486ae368b03208b0bdff716e124aecd502
amjadmajid <amjad.y.majid@gmail.com>
2024-11-08 15:01:55 +0100

    add babyGPT extractor code and the model's root directory

---
+++ extract_inference_model.py
@@ -0,0 +1,46 @@
+# this script is used to extract the inference model from the trained model and save it to a file
+# This is done to remove the optimizer and scaler from the model and save only the model's state_dict
+# This is done to reduce the model's size and make it easier to load the model for inference
+
+import torch
+import logging
+from model import TransformerModel
+from config import ConfigHandler
+from utils import load_checkpoint_
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+
+    # Load configurations
+    try:
+        # TODO: this is not a good approach. Enable the user to load the desired checkpoint
+        config = ConfigHandler.load("config/default_config.yaml")
+        config = ConfigHandler.load(Path(config.ckpt_dir)/ Path(config.ckpt_config))
+    except Exception as e:
+        logger.error(f"Error loading configuration: {e}")
+        exit(1)
+
+    logger.info(f"the selected device is automatically selected according to this device")
+    config.device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+    logger.info(f"The selected device is {config.device}")
+
+    # Initialize the model
+    model = TransformerModel(
+        n_head=config.n_head,
+        vocab_size=config.vocab_size,
+        n_embd=config.n_embd,
+        seq_len=config.seq_len,
+        device=config.device,
+        dropout_rate=config.dropout_rate,
+        n_blocks=config.n_blocks, 
+        decoder=True
+    ).to(config.device)
+
+    model_pth = Path(config.ckpt_dir) / Path(config.ckpt_model)
+    load_checkpoint_(model, "_", "_",  model_pth, config.device, inference=True)
+
+    # Save only the model's state_dict
+    torch.save(model.state_dict(), 'model/babyGPT/babyGPT_152M')
